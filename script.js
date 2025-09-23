a3e50c1072c6f2b363aab74aee21d86e934aa39a
SprintingSnail69 <your-email@example.com>
2025-09-22 20:28:25 -0500

    d

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let backgrounds = ["images/background_graffiti.png", "images/lumi_lobby.png"];
 let randomIndex = Math.floor(Math.random() * backgrounds.length);
-document.body.style.backgroundImage = `url('images/${backgrounds[randomIndex]}')`;
+document.body.style.backgroundImage = `url('${backgrounds[randomIndex]}')`;
